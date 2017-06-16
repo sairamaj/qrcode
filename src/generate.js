a93af67ecd900d9848c9d1dedba1ccd8d5b3ea39
sairama <sairamaj@hotmail.com>
2017-06-15 18:20:08 -0700

    added generating images

--- src/generate.js
+++ src/generate.js
@@ -1,5 +1,23 @@
 var qr = require('qr-image')
-var fs = require('fs')
+var fs = require('fs'), path = require('path')
 
-var png_string = qr.imageSync('I love QR!', { type: 'png' });
-fs.writeFileSync('qr.png',png_string)
+var mkdirp = require('mkdirp');
+
+mkdirp('images', function(err) { 
+});
+
+var baseUrl = 'http://dancedetails.s3-website-us-east-1.amazonaws.com/'
+fs.readdir('../web', function(err, files){
+    var htmlFiles = files
+                    .filter( f=> f.endsWith('html'))
+                    .filter( f => (f != 'error.html') && ( f != 'index.html') )
+    htmlFiles.forEach( htmlFile => {
+        var htmlUrl = baseUrl + htmlFile
+        var imageName =  path.join('images',path.basename(htmlFile, '.html') + '.png')
+        console.log(imageName)
+        console.log('generating: ' + htmlFile)
+        var imageData = qr.imageSync(htmlUrl, { type: 'png' });
+        fs.writeFileSync(imageName, imageData)        
+    })
+})
+
